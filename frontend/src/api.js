698653994ef13ab9f3ce8392461e5e81adeae9f3
Felipe Fiuza <ffiuzasantos@gmail.com>
2026-04-17 12:18:05 -0300

    feat: frontend apontando para backend de produção

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'https://medhub-backend-nlkg.onrender.com'
 })
 
 export async function getEspecialidades() {
