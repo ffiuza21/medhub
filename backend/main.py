deddeeeeff123c8908c42afee958a8a5b210a080
Felipe Fiuza <ffiuzasantos@gmail.com>
2026-04-17 12:43:48 -0300

    fix: CORS produção e título da página

--- backend/main.py
+++ backend/main.py
@@ -26,10 +26,11 @@ def conexao():
 
 #------------------ CONFIGURAR CORS FASTAPI ------------------
 origins = [
-   "http://localhost:5173",
-   "http://localhost:5174",
-   "http://127.0.0.1:5173",
-   "http://127.0.0.1:5174",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "https://medhub-gkks23mvr-ffiuzas-projects.vercel.app",
 ]
 
 # Middleware
